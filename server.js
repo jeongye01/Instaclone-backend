69c6354ddef7cfed62959b2543477be4b9959eba
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2021-09-29 03:12:43 +0900

    setup1

---
+++ server.js
@@ -0,0 +1,15 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query{
+    hello:String
+  }
+`
+
+
+const resolvers = {
+  Query: {
+    
+    hello: () => "hiiiii",
+  },
+};
