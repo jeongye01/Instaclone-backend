7e4d9580fd0af27524a00a6337b4925b141e7916
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2021-10-01 04:53:25 +0900

    hash

---
+++ users/users.mutations.js
@@ -0,0 +1,38 @@
+import client from "../client";
+import bcrypt from "bcrypt"; 
+export default {
+   Mutation:{
+     createAccount:async (_,{
+      firstName,
+      lastName,
+      username,
+      email,
+      password})=>{
+        //check in username or email are already on DB.
+        const existingUser=await client.user.findFirst({
+          where:{
+          OR:[
+            {
+             username,
+            },
+            {
+             email,
+            },
+          ],
+        },
+      });
+        //hash password
+      const uglyPassword=await bcrypt.hash(password,10);
+      return client.user.create({
+        data: {
+          username,
+          email,
+          firstName,
+          lastName,
+          password:uglyPassword,
+        },
+      });
+        //save and return the user
+      }
+   }
+};
