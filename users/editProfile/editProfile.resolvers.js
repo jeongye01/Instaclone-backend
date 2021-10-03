02d8d3c5e66d97b9123029411cc3da099d8a1aad
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2021-10-03 11:49:30 +0900

    changing avatar

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,6 @@
 import client from '../../client';
 import bcrypt from "bcrypt";
+import fs from "fs";
 
 import { protectedResolver } from '../users.utils';
 
@@ -11,8 +12,18 @@ export default {
       username,
       email,
       password:newPassword,
-      bio
+      bio,
+      avatar
     },{loggedInUser})=>{
+      let avatarUrl=null;
+      if(avatar){
+      const { filename, createReadStream } = await avatar;
+      const newFilename=`${loggedInUser.id}-${Date.now()}-${filename}`;
+      const readStream = createReadStream();
+      const writeStream=fs.createWriteStream(process.cwd()+"/uploads/"+newFilename);
+      readStream.pipe(writeStream);
+      avatarUrl=`http://localhost:4000/static/${newFilename}`;
+      }
       let uglyPassword=null;
       if(newPassword){
         uglyPassword=await bcrypt.hash(newPassword,10);
@@ -28,6 +39,7 @@ export default {
       email,
       bio,
       ...(uglyPassword&&{password:uglyPassword}),
+      ...(avatarUrl && {avatar:avatarUrl})
      
       }});
       if(updatedUser.id){
