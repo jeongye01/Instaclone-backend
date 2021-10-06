6b5a63a93d47d04db3015ef9d3cfed7f089edc21
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2021-10-07 00:01:48 +0900

    See Followers

--- users/seeProfile/seeProfile.resolvers.js
+++ users/seeProfile/seeProfile.resolvers.js
@@ -1,4 +1,14 @@
 import client from "../../client";
 export default {
-  
+  Query:{
+    seeProfile:async(_,{username})=>{
+      await client.user.findUnique({
+        where:{username},
+        include:{
+          following:true,
+          followers:true
+        }
+      });
+    }
+  }
 };
---
+++ users/unfollowUser/unfollowUser.typeDefs.js
@@ -0,0 +1,10 @@
+import {gql} from "apollo-server";
+export default gql`
+  type UnfollowUserResult{
+    ok:String!
+    error:String
+  }
+  type Mutation{
+    unfollowUser(username:String!):UnfollowUserResult!
+  }
+`;
